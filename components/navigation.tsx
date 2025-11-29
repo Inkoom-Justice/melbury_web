4dab907a9eaea24a016727f37f21e89e646f0c81
Justice Inkoom <justiceinkoom55@gmail.com>
2025-11-29 15:39:56 +0100

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { Menu, X } from "lucide-react"
-import Image from "next"
+{/* import Image from "next" */}
 
 export function Navigation() {
   const [scrolled, setScrolled] = useState(false)
@@ -36,7 +36,7 @@ export function Navigation() {
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex-shrink-0">
-            <Image
+            <img
               src={
                 scrolled
                   ? "mulberry-homes-logo-refined-black.png"
